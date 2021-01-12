107bb73b3b7d510866b17baf0b4a2b93ca8094f6
Osamu Okano <okano.osamu@mercari.com>
2021-01-12 15:30:51 +0900

    add open google image

--- main.go
+++ main.go
@@ -14,11 +14,12 @@ var (
 	os, ns  string
 	openURL = browser.OpenURL
 	readAll = clipboard.ReadAll
-	parse   = url.Parse
+	// parse   = url.Parse
+	parse = url.ParseRequestURI
 )
 
 func main() {
-	t := time.NewTicker(3 * time.Second)
+	t := time.NewTicker(1 * time.Second)
 	for {
 		select {
 		case <-t.C:
@@ -33,6 +34,7 @@ func do() {
 	if os != ns {
 		for _, n := range strings.Split(ns, "\n") {
 			openIfURL(n)
+			openGoogleImage(n)
 		}
 	}
 	os = ns
@@ -50,4 +52,19 @@ func openIfURL(n string) {
 
 func isTarget(u *url.URL) bool {
 	return u.Scheme == "https"
+}
+
+func openGoogleImage(n string) {
+	if len(n) <= 1 {
+		return
+	}
+	_, err := parse(n)
+	if err != nil {
+		url := "https://www.google.com/search?tbm=isch&q=" + n
+		log.Printf("%s", url)
+		err := openURL(url)
+		if err != nil {
+			log.Println("%w", err)
+		}
+	}
 }
